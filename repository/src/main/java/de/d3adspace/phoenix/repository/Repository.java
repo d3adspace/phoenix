595ed56d8f6dbf4e50b507edd0f401cda16d4646
Felix Klauke <info@felix-klauke.de>
2019-06-04 15:26:43 +0200

    Add find all and save methods.

--- repository/src/main/java/de/d3adspace/phoenix/repository/Repository.java
+++ repository/src/main/java/de/d3adspace/phoenix/repository/Repository.java
@@ -1,5 +1,8 @@
 package de.d3adspace.phoenix.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /**
  * A repository defines the entry point for managing an entity.
  */
@@ -11,4 +14,17 @@ public interface Repository<EntityType> {
      * @return The class of the entity.
      */
     Class<EntityType> getEntityClass();
+
+    /**
+     * Find all entities.
+     */
+    List<EntityType> findAll();
+
+    /**
+     * Save the given entity.
+     *
+     * @param entity The entity.
+     * @return The saved entity.
+     */
+    EntityType save(EntityType entity);
 }
